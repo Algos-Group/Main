8181fb78e132102a7720a7f0a0c874bd12fa7daa
Ryan Fleet <ryanfleeter24@gmail.com>
2022-11-16 20:53:05 -0600

    messed with dash styling

--- toy_collector/client/src/components/user/Dashboard.jsx
+++ toy_collector/client/src/components/user/Dashboard.jsx
@@ -18,28 +18,28 @@ const Dashboard = ({ user }) => {
       <>
       <h1>Hiiii, {user.username}</h1>
       <div className="d-flex justify-content-center">
-        <Card style={{ width: '12rem' }}>
-          <Card.Img className="categoryImg" src={toy} alt='wheelchair' />
+        <Card className='m-2' style={{ width: '12rem' }}>
+          <Card.Img className="categoryImg w-auto m-3" src={toy} alt='wheelchair' />
           <Card.Body>
             <Link to="/addToy"><Button variant="dark">Add a Toy</Button></Link>
           </Card.Body>
         </Card>
-        <Card style={{ width: '12rem' }}>
-          <Card.Img className="categoryImg" src={shelf} alt='wheelchair' />
+        <Card className='m-2' style={{ width: '12rem' }}>
+          <Card.Img className="categoryImg w-auto m-2" src={shelf} alt='wheelchair' />
           <Card.Body>
             <Link to="/allToys"><Button variant="dark">All Toys</Button></Link>
           </Card.Body>
         </Card>
-        <Card style={{ width: '12rem' }}>
-          <Card.Img className="categoryImg" src={reserve} alt='wheelchair' />
+        <Card className='m-2' style={{ width: '12rem' }}>
+          <Card.Img className="categoryImg w-auto" src={reserve} alt='wheelchair' />
           <Card.Body>
             <Link to="/availableToys"><Button variant="dark">Available Toys</Button></Link>
           </Card.Body>
         </Card>
-        <Card style={{ width: '12rem' }}>
-          <Card.Img className="categoryImg" src={sold} alt='wheelchair' />
+        <Card className='m-2 d-flex align-self' style={{ width: '12rem' }}>
+          <Card.Img className="categoryImg w-auto m-4" src={sold} alt='wheelchair' />
           <Card.Body>
-            <Link to="/reservedToys"><Button variant="dark">Reserved Toys</Button></Link>
+            <Link to="/reservedToys"><Button className='mt-5' variant="dark">Reserved Toys</Button></Link>
           </Card.Body>
         </Card>
       </div>
